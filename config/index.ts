949b639d2f34087cac719a71817cd17c476f9751
Peter Arnold <parnold@retrieve.com>
2021-05-07 12:22:41 -0400

    RESET REPO

--- config/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Configuration
- */
-export const SERVER = "https://tolerant-sparrow-62.hasura.app/v1/graphql";
-export const WEB_SOCKET_LINK =
-  "wss://tolerant-sparrow-62.hasura.app/v1/graphql";
--- modules.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace NodeJS {
-  export interface ProcessEnv {
-    NEXT_PUBLIC_FLAGS_CLIENT_ID: string;
-  }
-}
