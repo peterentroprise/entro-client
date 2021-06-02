64c69bb7220841088a18274fdf3825642bf9a8df
Peter Arnold <peter@entroprise.com>
2021-06-02 12:01:52 -0400

    asd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,15 @@ import "@fontsource/mulish/600.css";
 import theme from "../theme";
 import { AppProps } from "next/app";
 
+import LogRocket from "logrocket";
+import setupLogRocketReact from "logrocket-react";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  if (typeof window !== "undefined") {
+    LogRocket.init("xjwz8l/entro-web");
+    setupLogRocketReact(LogRocket);
+  }
+
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
